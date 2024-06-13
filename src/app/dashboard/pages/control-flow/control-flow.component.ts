c5e18ae29976f0a0f4771b083135b9a85d9233c6
David Salazar <davidmartin.salazar@globant.com>
2024-06-13 17:08:27 -0500

    add dashboard template and firts control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+type Grade = 'A'|'B'|'F';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
 export default class ControlFlowComponent {
+
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Quik', 'React']);
+  public frameworks2 = signal([]);
+
+  public toggleContent() {
+    this.showContent.update( value => !value )
+  }
 
 }
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '../../app.routes';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidemenu.component.html',
+  styles: ``,
+})
+export class SidemenuComponent {
+
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+
+  constructor() {
+
+  }
+}
