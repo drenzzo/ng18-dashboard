d4d0d8284694c8b4098b68d1859069776ab85ae5
David Salazar <davidmartin.salazar@globant.com>
2024-06-14 15:28:06 -0500

    feat: required input && change detection onpush

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './title.component.html',
   styles: ``
 })
 export class TitleComponent {
+
+  @Input({ required: true }) title!: string;
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
 
 }
